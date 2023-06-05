39d429a77233833825c9423dfca1be1bb1ec7e2e
Heitor negromonte <heiitornoc@gmail.com>
2023-06-05 17:09:53 -0300

    continue folder structure + layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import { Inter } from 'next/font/google'
+import  Navbar  from '@/components/navbar/Navbar'
+import  Footer  from '@/components/footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,7 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <div>
+      <div>©2023 htrz. All rights reserved.</div>
+      <div></div>
+    </div>
+  )
+}
